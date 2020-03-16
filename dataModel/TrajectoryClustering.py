8845d48626d0d328e567db04b94c9c9f675c842e
Lexie1216 <973895978@qq.com>
2020-03-16 22:18:51 +0800

    驻留点聚类

--- dataModel/TrajectoryClustering.py
+++ dataModel/TrajectoryClustering.py
@@ -41,14 +41,14 @@ userNum=listType.size
 
 start = time.clock()
 
-cluster_res=pd.DataFrame(columns=['imsi', 'longitude', 'latitude','plot','date_time','cluster'])
+cluster_res=pd.DataFrame(columns=['imsi', 'longitude', 'latitude','newPlot','date_time','cluster'])
 
-for i in range(0,userNum):
+for i in range(0,2):
     tempUser=df[df['imsi'].isin([listType[i]])]
     # 聚类
     df_clustering = ST_DBSCAN(tempUser, spatial_threshold, temporal_threshold, min_neighbors)
     cluster_res = cluster_res.append(df_clustering)
-    # # 可视化
+    # 可视化
     # plotFeature(df_clustering)
 
 # 聚类操作的时间
@@ -60,11 +60,29 @@ print('finish all in %s s' % str(end - start))
 # cluster_res.drop(['date_time'], inplace=True, axis=1)
 
 
-stayPoint=pd.DataFrame(columns=('imsi','longitude','latitude','plot','start','end'))
-travelPoint=pd.DataFrame(columns=('imsi', 'longitude', 'latitude','plot','date_time'))
+stayPoint=pd.DataFrame(columns=('imsi','longitude','latitude','newPlot','start','end'))
+travelPath=pd.DataFrame(columns=('imsi', 'start', 'end','startPlot','endPlot'))
+
+tempRes=pd.DataFrame(columns=('imsi','longitude','latitude','newPlot','start','end'))
 
 
-for i in range(0,userNum):
+"""""
+处理聚类结果
+
+1.stayPoint:
+    将每一类的数据计算成一条新的数据（类别不为-1）
+    imsi-longitude-latitude-plot-start-end
+
+2.tempRes:
+    对于类别为-1的不需要计算，直接拼接
+    将上述结果以人为单位，按照start进行排序
+
+3.travelPath:
+    遍历tempRes将每一条的start和下一条的end形成一条新的数据
+
+"""""
+
+for i in range(0,2):
     tempUser = cluster_res[cluster_res['imsi'].isin([listType[i]])]
     userID=tempUser.iloc[0,0]
 
@@ -83,37 +101,50 @@ for i in range(0,userNum):
             end=tempCluster['date_time'].max()
             lat=round(tempCluster['latitude'].mean(),8)
             lon=round(tempCluster['longitude'].mean(),8)
-            plot=round(tempCluster['plot'].mode()[0],0)
+            plot=round(tempCluster['newPlot'].mode()[0],0)
 
             stay_tmp = pd.DataFrame([userID,lon,lat,plot,start,end]).T
             # 修改当前数据的column一致
             stay_tmp.columns = stayPoint.columns
             # 把两个dataframe合并，需要设置 ignore_index=True
             stayPoint = pd.concat([stayPoint, stay_tmp], ignore_index=True)
+            tempRes = pd.concat([tempRes, stay_tmp], ignore_index=True)
 
         # 出行点
         else:
+            tempCluster['start']=tempCluster['date_time']
+            tempCluster['end'] = tempCluster['date_time']
             tempCluster.drop(['cluster'], inplace=True, axis=1)
+            tempCluster.drop(['date_time'], inplace=True, axis=1)
 
-            travelPoint = pd.concat([travelPoint, tempCluster], ignore_index=True)
+            tempRes = pd.concat([tempRes, tempCluster], ignore_index=True)
 
-
-
-# print(cluster_res.info())
-# print(travelPoint.info())
-# print(stayPoint.info())
-
-# 以人为单位，按时间正序排序
-cluster_res = cluster_res.sort_values(['imsi', 'date_time'])
-travelPoint=travelPoint.sort_values(['imsi','date_time'])
 stayPoint=stayPoint.sort_values(['imsi','start'])
+tempRes=tempRes.sort_values(['imsi','start'])
+
+for i in range(0,2):
+    tempUser = tempRes[tempRes['imsi'].isin([listType[i]])]
+    userID = tempUser.iloc[0, 0]
+    entryNum=tempUser.shape[0]
+    for j in range(0,entryNum-1):
+        start=tempUser.iloc[j, 5]
+        end=tempUser.iloc[j+1, 4]
+        startPlot=tempUser.iloc[j, 3]
+        endPlot=tempUser.iloc[j+1, 3]
+
+        travel_tmp = pd.DataFrame([userID, start, end, startPlot, endPlot]).T
+        # 修改当前数据的column一致
+        travel_tmp.columns = travelPath.columns
+
+        travelPath = pd.concat([travelPath, travel_tmp], ignore_index=True)
 
 
-# 输出csv
-cluster_res.to_csv('../data/clusterResult.csv', index=False)
-travelPoint.to_csv('../data/travelPoint.csv', index=False)
-stayPoint.to_csv('../data/stayPoint.csv', index=False)
+cluster_res.to_csv('../resultData/clusterRes.csv', index=False)
+stayPoint.to_csv('../resultData/stayPoint.csv', index=False)
+travelPath.to_csv('../resultData/travelPath.csv', index=False)
 
+
+# tempRes.to_csv('../resultData/tempRes.csv', index=False)
 
 
 
