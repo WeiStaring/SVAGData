b3c7664406ace69679fb297acb195032b06f7e20
cherrybrandy <741989550@qq.com>
2020-03-21 10:35:51 +0800

    更新TripModeResult.csv及TripMode.py

--- tripmode/TripMode .py
+++ tripmode/TripMode .py
@@ -1,17 +1,21 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
+# coding: utf-8
 
+# -*- coding: utf-8 -*-
 from urllib.request import urlopen
 import json
 import hashlib
 import requests
+
 import pandas as pd
 import numpy as np
 
 np.set_printoptions(suppress=True)
 
-MyAK='jt2ebs4tgp1TyHowp3Ol4sOheMGH8S0u'
-# MySK=''
+#MyAK='jt2ebs4tgp1TyHowp3Ol4sOheMGH8S0u'
+# MyAK='Pju1uy3aoutZxA16NX0xMgtwxX1xoqhs'
+MySK=''
+
 
 #调用百度API判断交通方式
 def getduration(origin_path,result_path,MyAK,mytime):
@@ -19,10 +23,10 @@ def getduration(origin_path,result_path,MyAK,mytime):
     url_walk = "http://api.map.baidu.com/directionlite/v1/walking?origin="+str(origin_path[0])+","+str(origin_path[1])+"&destination="+str(result_path[0])+","+str(result_path[1])+"&tactics=3&ak="+MyAK
     url_ride="http://api.map.baidu.com/directionlite/v1/riding?origin="+str(origin_path[0])+","+str(origin_path[1])+"&destination="+str(result_path[0])+","+str(result_path[1])+"&tactics=3&ak="+MyAK
     url_bus="http://api.map.baidu.com/directionlite/v1/transit?origin="+str(origin_path[0])+","+str(origin_path[1])+"&destination="+str(result_path[0])+","+str(result_path[1])+"&tactics=3&ak="+MyAK
-    print(url_bus)
-    print(url_drive)
-    print(url_ride)
-    print(url_walk)
+#     print(url_drive)
+#     print(url_walk)    
+#     print(url_ride)
+#     print(url_bus)
     #从API读取驾驶数据
     response0 = requests.get(url_drive)
     answer0 = response0.json()
@@ -58,11 +62,14 @@ def getduration(origin_path,result_path,MyAK,mytime):
     else:
         answer[3]=answer3['result']['routes'][0]['duration']
     print(answer)
-    y=0
+    print(mytime)
+    
+    y=0        
+    x=float("inf")
     for i in answer:
-        x=float("inf")
         if abs(mytime-answer[i])<x:
             y=i+1
+            x=abs(mytime-answer[i])
     print(y)
     return y
 
@@ -70,14 +77,16 @@ df_travel=pd.read_csv("travelPath.csv")
 df_travel
 
 #设置显示小数，转换换为时间戳格式
-pd.set_option('display.float_format', lambda x: '%.3f' % x)
-startstamp = pd.to_datetime(df_travel['start'])
-df_travel['startstamp'] = (startstamp.values - np.datetime64('1970-01-01T08:00:00Z')) / np.timedelta64(1, 'ms')
-endstamp = pd.to_datetime(df_travel['end'])
-df_travel['endstamp'] = (endstamp.values - np.datetime64('1970-01-01T08:00:00Z')) / np.timedelta64(1, 'ms')
-df_travel['mytime']=df_travel['endstamp']-df_travel['startstamp']
+# pd.set_option('display.float_format', lambda x: '%.3f' % x)
+# startstamp = pd.to_datetime(df_travel['start'])
+# df_travel['startstamp'] = (startstamp.values - np.datetime64('1970-01-01T08:00:00Z')) / np.timedelta64(1, 'ms')
+# endstamp = pd.to_datetime(df_travel['end'])
+# df_travel['endstamp'] = (endstamp.values - np.datetime64('1970-01-01T08:00:00Z')) / np.timedelta64(1, 'ms')
+df_travel['mytime']=(df_travel['end']-df_travel['start'])/1000
+df_travel
 
 df_station=pd.read_csv("newStation.csv")
+df_station
 
 #获取起点坐标
 def getstartpoint(origin_path,newPlot):
@@ -92,11 +101,20 @@ def getendpoint(result_path,newPlot):
     return result_path
 
 #遍历获取出行方式
+# origin_path=[0,0]
+# result_path=[0,0]
+# answer={0:0,1:0,2:0,3:0}
+# getstartpoint(df_travel.loc[0][3])
+# getendpoint(df_travel.loc[0][4])
+# mytime=df_travel.loc[0][7]
+# getduration(getstartpoint(df_travel.loc[0][3]),getendpoint(df_travel.loc[0][4]),MyAK,mytime)
+
 for item in df_travel.iterrows():
+    #遍历获取出行方式
     origin_path=[0,0]
     result_path=[0,0]
     answer={0:0,1:0,2:0,3:0}
-    mytime=df_travel.loc[item[0]][7]
+    mytime=df_travel.loc[item[0]][5]
     origin_path=getstartpoint(origin_path,df_travel.loc[item[0]][3])
     result_path=getendpoint(result_path,df_travel.loc[item[0]][4])
     df_travel.loc[[item[0]],'startlatitude']=origin_path[1]
@@ -106,15 +124,17 @@ for item in df_travel.iterrows():
     res=getduration(origin_path,result_path,MyAK,mytime)
     df_travel.loc[[item[0]],'type']=res
 
-
+df_travel
 
 df_1=df_travel
-del df_1['startstamp']
-del df_1['endstamp']
+# del df_1['startstamp']
+# del df_1['endstamp']
 del df_1['mytime']
+# df_1.to_csv('Result.csv')
 
+df_1
 
-df_1.to_csv('TripModeResult.csv',index=None)
+df_1.to_csv( 'TripModeResult.csv',index=None)
 
 
 
